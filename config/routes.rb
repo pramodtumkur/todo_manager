f1dc8986e1f1fe1a9390a1c028bfb78cbda0d186
pramod/tumkur <tcpramodhere@gmail.com>
2021-11-13 13:19:06 +0530

    added user management

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-post "users/create", to: "users#create"
-
 post "users/login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
